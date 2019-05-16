5168af54289750de5fb04827d2e72430dda717da
pgarces <gspablo@gmail.com>
2019-05-16 15:15:16 +0200

    Add languages

---
+++ catalog/includes/languages/italiano/modules/payment/pagantis.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Pagantis payment module for oscommerce
+ *
+ * @package     Pagantis
+ * @author      Integrations team <integrations@pagantis.com>
+ * @copyright   Copyright (c) 2019  Pagantis (http://www.pagantis.com)
+ *
+ * @license     Released under the GNU General Public License
+ *
+ */
+
+  global $customer_id, $order;
+
+  define('MODULE_PAYMENT_PAGANTIS_TEXT_ADMIN_TITLE', 'Pagantis');
+  define('MODULE_PAYMENT_PAGANTIS_PANEL_TITLE', 'Enable Module');
+  define('MODULE_PAYMENT_PAGANTIS_LANG_CODE', 'it');

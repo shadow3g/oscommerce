e6d9226e556b22453b54b497756a515332ceebf6
rmairnleal <raul.marin.leal@gmail.com>
2019-06-05 15:30:40 +0200

    default language Español

--- config/languages.sql
+++ config/languages.sql
@@ -5,8 +5,9 @@ INSERT INTO products_options select 3,2,'Modelo';
 INSERT INTO products_options select 4,2,'Memoria';
 INSERT INTO products_options select 5,2,'Version';
 INSERT INTO products_description select products_id, 2, products_name, products_description, products_url, products_viewed from products_description where language_id = 1;
+UPDATE configuration SET configuration_value = 'es' where configuration_key = 'DEFAULT_LANGUAGE';
 
-INSERT INTO languages (languages_id, name, code, image, directory, sort_order) values (3, 'Francés', 'fr', 'icon.gif', 'french',2);
+INSERT INTO languages (languages_id, name, code, image, directory, sort_order) values (3, 'Francés', 'fr', 'icon.gif', 'french',3);
 INSERT INTO products_options select 1,3,'Color';
 INSERT INTO products_options select 2,3,'Size';
 INSERT INTO products_options select 3,3,'Model';
@@ -14,7 +15,7 @@ INSERT INTO products_options select 4,3,'Memory';
 INSERT INTO products_options select 5,3,'Version';
 INSERT INTO products_description select products_id, 3, products_name, products_description, products_url, products_viewed from products_description where language_id = 1;
 
-INSERT INTO languages (languages_id, name, code, image, directory, sort_order) values (4, 'Italiano', 'it', 'icon.gif', 'italiano',2);
+INSERT INTO languages (languages_id, name, code, image, directory, sort_order) values (4, 'Italiano', 'it', 'icon.gif', 'italiano',4);
 INSERT INTO products_options select 1,4,'Color';
 INSERT INTO products_options select 2,4,'Size';
 INSERT INTO products_options select 3,4,'Model';
@@ -22,7 +23,7 @@ INSERT INTO products_options select 4,4,'Memory';
 INSERT INTO products_options select 5,4,'Version';
 INSERT INTO products_description select products_id, 4, products_name, products_description, products_url, products_viewed from products_description where language_id = 1;
 
-INSERT INTO languages (languages_id, name, code, image, directory, sort_order) values (5, 'Portugues', 'pt', 'icon.gif', 'portugues',2);
+INSERT INTO languages (languages_id, name, code, image, directory, sort_order) values (5, 'Portugues', 'pt', 'icon.gif', 'portugues',5);
 INSERT INTO products_options select 1,5,'Color';
 INSERT INTO products_options select 2,5,'Size';
 INSERT INTO products_options select 3,5,'Model';

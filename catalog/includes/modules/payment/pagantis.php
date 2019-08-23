8b985d02ef6ba5a92e30e124644bce0cb28aa2f8
pgarces <gspablo@gmail.com>
2019-08-23 10:55:02 +0200

    Updated version

--- catalog/includes/modules/payment/pagantis.php
+++ catalog/includes/modules/payment/pagantis.php
@@ -67,7 +67,7 @@ class pagantis
      */
     public function __construct()
     {
-        $this->version = '8.1.1';
+        $this->version = '8.1.2';
         $this->code = 'pagantis';
         $this->sort_order = 0;
         $this->description = $this->getDescription();
